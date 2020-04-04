cef557ee1b1a10022e87c2e7e549770c0fc77921
Dooski <dmckeon495@gmail.com>
2020-04-04 10:37:06 -0400

    added id's for pic of day date and button; added links to jquery and moment.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //javascript for website functionality
+console.log("hey lmao")
+
+var userName = localStorage.getItem(userName);
+var picDate = $("#picDate").val()
+
+if (picDate === null) {
+    $("#today").valueAsDate = new Date();
+}
+
+$("#picSearchBtn").on("click", function () {
+
+})
+
+$(document).on('keypress', function (e) {
+    if (userName = null) {
+        if (e.which == 13) {
+            alert('You pressed enter!');
+        }
+
+        $("#9amText").each(function () {
+            var value = $(this).val();
+            localStorage.setItem(userName, value);
+        });
+    }
+})
